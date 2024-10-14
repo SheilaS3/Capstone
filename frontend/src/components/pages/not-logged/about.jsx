5e68193aceb40abc1b96dfb434a23f823760e244
SheilaS3 <sheilaportu@hotmail.com>
2024-10-15 00:01:36 +0200

    Added styles to about

--- frontend/src/components/pages/not-logged/about.jsx
+++ frontend/src/components/pages/not-logged/about.jsx
@@ -7,17 +7,17 @@ export default class About extends Component {
                 <div className='about-wrapper'>
                     <div className='left-column'>                       
                         <div>
-                            We are here to help your company keep client's info updated as part of the due diligence measures application. 
+                            We are here to help your company keep <b>client's info updated</b> as part of the due diligence measures application. 
                         </div>
                     </div>
                     
                     <div className='right-column'>
-                       <div>
-                            Set up alerts to get informed when one of your client's KYC info has to be updated based on their assigned risk.
+                       <div className='top-paragraph'>
+                            Set up alerts to get informed when one of your <b>client's KYC</b> info has to be <b>updated</b> based on their assigned risk.
                        </div>
 
-                       <div>
-                        Anticipate to your client's ID expiry date so you can request for the new ones.
+                       <div className='bottom-paragraph'>
+                        Anticipate to your client's <b>ID expiry date</b> so you can request for the new ones.
                        </div>
                     </div>
                 </div>
