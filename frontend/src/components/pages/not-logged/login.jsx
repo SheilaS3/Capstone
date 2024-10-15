6932f638661db95faaa05ad6ed8f6cfcf8bfdc98
SheilaS3 <sheilaportu@hotmail.com>
2024-10-15 02:02:17 +0200

    Added styles to login

--- frontend/src/components/pages/not-logged/login.jsx
+++ frontend/src/components/pages/not-logged/login.jsx
@@ -4,35 +4,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default class Login extends Component {
     render() {
         return (
-            <div>
-            <h1>LOGIN TO YOUR ACCOUNT</h1>
-
-            <form className="login-form-wrapper">
-                <div className="form-group">
-                    <FontAwesomeIcon icon="envelope" />
-
-                    <input 
-                        type="email"
-                        name="email"
-                        placeholder="Your email"
-                    />
+            <div className='login-wrapper'>
+                <div className='title'>
+                    <h1>LOGIN TO YOUR ACCOUNT</h1>
                 </div>
 
-                <div className="form-group">
-                    <FontAwesomeIcon icon="lock" />
+                <form className="login-form-wrapper">
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="envelope" />
 
-                    <input 
-                        type="password"
-                        name="password"
-                        placeholder="Your password"
-                    />
-                </div>
+                        <input 
+                            type="email"
+                            name="email"
+                            placeholder="Your email"
+                        />
+                    </div>
 
-                <button className="btn" type="submit">
-                    Login
-                </button>
-            </form>                    
-        </div>
+                    <div className="form-group">
+                        <FontAwesomeIcon icon="lock" />
+
+                        <input 
+                            type="password"
+                            name="password"
+                            placeholder="Your password"
+                        />
+                    </div>
+
+                    <button className="btn" type="submit">
+                        Login
+                    </button>
+                </form>                    
+            </div>
         );
     }
 }
