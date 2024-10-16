2cf9f55136a1b8319b9fa49b27c255464f68d32a
SheilaS3 <sheilaportu@hotmail.com>
2024-10-16 20:49:03 +0200

    Added Dashboard

--- frontend/src/components/pages/logged/dashboard.jsx
+++ frontend/src/components/pages/logged/dashboard.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className='dashboard-wrapper'>
-                DASHBOARD
+                <Link
+                    className='link' 
+                    to="/clients"
+                >
+                    <h1>CLIENTS</h1>
+                </Link>
+
+                <Link 
+                    className='link'
+                    to="/checker"
+                >
+                    <h1>CHECKER</h1>
+                </Link>
+
+                <Link
+                    className='link'
+                    to="/new-client"
+                >
+                    <h1>NEW CLIENT</h1>
+                </Link>
             </div>
         );
     }
