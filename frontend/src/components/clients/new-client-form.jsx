057899a17c1176e1096f001914ff46f4ddb94e2e
SheilaS3 <sheilaportu@hotmail.com>
2024-10-16 23:28:26 +0200

    Added event handlers to New Client Form

--- frontend/src/components/clients/new-client-form.jsx
+++ frontend/src/components/clients/new-client-form.jsx
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
 
 export default class NewClientForm extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            assigned_risk: "Low",
+            activity: "",
+            contract_date: "",
+            country: "",
+            country_risk: "No",
+            funds_origin: "",
+            id_number: "",
+            id_number_expiry_date: "",
+            lastname: "",
+            name: "",
+            pep: "No",
+            person_type: "Natural"
+        }
+
+        this.handleSuccessfullClientSubmision = this.handleSuccessfullClientSubmision.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        this.handleSuccessfullClientSubmision(this.state);
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSuccessfullClientSubmision(client) {
+        console.log("client from client form", client);
+    }
+
     render() {
         return (
             <div className='new-client-form-wrapper'>
@@ -9,7 +47,10 @@ export default class NewClientForm extends Component {
                         <h1>NEW CLIENT FORM</h1>
                     </div>
 
-                    <form className='new-client-form'>
+                    <form
+                        onSubmit={this.handleSubmit} 
+                        className='new-client-form'
+                    >
                         <div className='column'>
                             <div className="form-group">
                                 <div className='entry-name'>
@@ -17,8 +58,11 @@ export default class NewClientForm extends Component {
                                 </div>
 
                                 <input 
+                                    onChange={this.handleChange}
                                     type="text"
-                                    name="client-name"
+                                    name="name"
+                                    value={this.state.name}
+                                    autoComplete="new-text"
                                 />
                             </div>
 
@@ -27,9 +71,11 @@ export default class NewClientForm extends Component {
                                     Lastname:
                                 </div>
 
-                                <input 
+                                <input
+                                    onChange={this.handleChange} 
                                     type="text"
-                                    name="client-lastname"
+                                    name="lastname"
+                                    value={this.state.lastname}
                                 />
                             </div>
 
@@ -39,8 +85,10 @@ export default class NewClientForm extends Component {
                                 </div>
                                 
                                 <select
-                                    name='person-type'
-                                    className='select-element'  
+                                    onChange={this.handleChange}
+                                    name='person_type'
+                                    className='select-element'
+                                    value={this.state.person_type}  
                                 >
                                     <option value="Natural">Natural</option>
                                     <option value="Legal">Legal</option>
@@ -53,8 +101,10 @@ export default class NewClientForm extends Component {
                                 </div>
 
                                 <input 
+                                    onChange={this.handleChange}
                                     type="text"
-                                    name="client-id"
+                                    name="id_number"
+                                    value={this.state.id_number}
                                 />
                             </div>
 
@@ -63,10 +113,12 @@ export default class NewClientForm extends Component {
                                     ID Expiry Date:
                                 </div>
 
-                                <input 
+                                <input
+                                    onChange={this.handleChange} 
                                     type="text"
-                                    name="client-id-expiry-date"
+                                    name="id_number_expiry_date"
                                     placeholder='YYYY-MM-DD'
+                                    value={this.state.id_number_expiry_date}
                                 />
                             </div>
 
@@ -75,9 +127,12 @@ export default class NewClientForm extends Component {
                                     Country:
                                 </div>
 
-                                <input 
+                                <input
+                                    onChange={this.handleChange} 
                                     type="text"
                                     name="country"
+                                    value={this.state.country}
+                                    autoComplete="new-text"
                                 />
                             </div>
 
@@ -87,8 +142,10 @@ export default class NewClientForm extends Component {
                                 </div>
                                 
                                 <select
-                                    name='risk-country'
-                                    className='select-element'  
+                                    onChange={this.handleChange} 
+                                    name='country_risk'
+                                    className='select-element'
+                                    value={this.state.country_risk}  
                                 >
                                     <option value="Yes">Yes</option>
                                     <option value="No">No</option>
@@ -103,8 +160,10 @@ export default class NewClientForm extends Component {
                                 </div>
 
                                 <input 
+                                    onChange={this.handleChange} 
                                     type="text"
                                     name="activity"
+                                    value={this.state.activity}
                                 />
                             </div>
 
@@ -114,8 +173,10 @@ export default class NewClientForm extends Component {
                                 </div>
 
                                 <input 
+                                    onChange={this.handleChange} 
                                     type="text"
-                                    name="funds-origin"
+                                    name="funds_origin"
+                                    value={this.state.funds_origin}
                                 />
                             </div>
 
@@ -125,8 +186,10 @@ export default class NewClientForm extends Component {
                                 </div>
                                 
                                 <select
+                                    onChange={this.handleChange} 
                                     name='pep'
-                                    className='select-element'  
+                                    className='select-element'
+                                    value={this.state.pep}  
                                 >
                                     <option value="Yes">Yes</option>
                                     <option value="No">No</option>
@@ -138,10 +201,12 @@ export default class NewClientForm extends Component {
                                     Contract Date:
                                 </div>
 
-                                <input 
+                                <input
+                                    onChange={this.handleChange} 
                                     type="text"
-                                    name="contract-date"
+                                    name="contract_date"
                                     placeholder='YYYY-MM-DD'
+                                    value={this.state.contract_date}
                                 />
                             </div>
 
@@ -151,8 +216,10 @@ export default class NewClientForm extends Component {
                                 </div>
                                 
                                 <select
-                                    name='assigned-risk'
-                                    className='select-element'  
+                                    onChange={this.handleChange} 
+                                    name='assigned_risk'
+                                    className='select-element'
+                                    value={this.state.assigned_risk}  
                                 >
                                     <option value="Low">Low</option>
                                     <option value="Medium">Medium</option>
